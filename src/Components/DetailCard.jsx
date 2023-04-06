d6c7bfc754c44c8d8d0b0fceefe21a207e791048
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-06 15:44:09 -0300

    Se agrega fetch para un dentista

--- src/Components/DetailCard.jsx
+++ src/Components/DetailCard.jsx
@@ -1,24 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ScheduleFormModal from "./ScheduleFormModal";
 import styles from "./DetailCard.module.css";
+import { useParams } from "react-router-dom";
 
 const DetailCard = () => {
 
+  const [dentist, setDentist] = useState(undefined);
+  const params = useParams();
+
+  const idDentist = params.id
+
+  const getDentist = async () =>{
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/?id=${idDentist}`);
+    const data = await response.json();
+    setDentist(data[0])
+  }
+
   useEffect(() => {
-    //Nesse useEffect, você vai fazer um fetch na api passando o 
-    //id do dentista que está vindo do react-router e carregar os dados em algum estado
-  }, []);
+    getDentist();
+  }, [params])
+
   return (
     //As instruções que estão com {''} precisam ser 
     //substituídas com as informações que vem da api
     <>
-      <h1>Detail about Dentist {'Nome do Dentista'} </h1>
+      <h1>Detail about Dentist </h1>
       <section className="card col-sm-12 col-lg-6 container">
         {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
-        <div
-          className={`card-body row`}
-        >
+          <div className={`card-body row`}>
+
           <div className="col-sm-12 col-lg-6">
             <img
               className="card-img-top"
@@ -28,12 +39,18 @@ const DetailCard = () => {
           </div>
           <div className="col-sm-12 col-lg-6">
             <ul className="list-group">
-              <li className="list-group-item">Nome: {'Nome do Dentista'}</li>
+              <li className="list-group-item">Nombre: {dentist?.name}</li>
               <li className="list-group-item">
-                Sobrenome: {'Sobrenome do Dentista'}
+                Apellido: {dentist?.username}
               </li>
               <li className="list-group-item">
-                Usuário: {'Nome de usuário do Dentista'}
+                Email: {dentist?.email}
+              </li>
+              <li className="list-group-item">
+                Teléfono: {dentist?.phone}
+              </li>
+              <li className="list-group-item">
+                Website: {dentist?.website}
               </li>
             </ul>
             <div className="text-center">
@@ -49,7 +66,7 @@ const DetailCard = () => {
               </button>
             </div>
           </div>
-        </div>
+        </div>      
       </section>
       <ScheduleFormModal />
     </>
