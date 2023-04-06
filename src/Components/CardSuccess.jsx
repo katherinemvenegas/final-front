dfd858ee892e9a8b8216a653b7fbf59bad63eccc
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-06 20:04:00 -0300

    Se agrega respuesta de form

---
+++ src/Components/CardSuccess.jsx
@@ -0,0 +1,7 @@
+const CardSuccess = ({ name }) => {
+    return (
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Gracias {name}, te contactaremos cuando antes vía mail</h1>
+    );
+};
+
+export default CardSuccess;
