25221c20a096a03471ff26a09986a08592dbc4c7
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-07 16:53:03 -0300

    Se agrega ThemeContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import ThemeContext from "./context";
+
+export const themes = {
+  light: {
+    font: "black",
+    background: "white"
+  },
+  dark: {
+    font: "white",
+    background: "black"
+  }
+}
 
 function App() {
+  const [theme, setTheme] = useState(themes.light)
+
+  const handleChangeTheme = () => {
+    console.log(theme);
+    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light)
+  }
   return (
     <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar a classe dark ou light */}
-      <div className={`app light}`}>
-        <Navbar />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
+      <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
+        <div>
+          <Navbar />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 }
