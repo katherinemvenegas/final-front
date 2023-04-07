85fd5504e427751bbcb8be30856c40a7d4310552
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-07 19:05:30 -0300

    Agrego logica de contextProvider

--- src/context.js
+++ src/context.js
@@ -1,5 +1,49 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const GlobalContext = createContext();
+export const GlobalContext = createContext();
 
-export default GlobalContext;
+export const themes = {
+    light: {
+      font: "black",
+      background: "white"
+    },
+    dark: {
+      font: "white",
+      background: "black"
+    }
+  }
+
+const GlobalContextProvider = ({children}) => {
+
+        const [dentists, setDentists] = useState([]);
+      
+        const url = 'https://jsonplaceholder.typicode.com/users';
+      
+        const getAllDentists = async () => {
+          const response = await fetch(url);
+          const data = await response.json();
+          setDentists(data)
+        }
+        useEffect(() => {
+          getAllDentists();
+        }, [])
+      
+        const localTheme = JSON.parse(localStorage.getItem('theme'));
+        const [theme, setTheme] = useState(localTheme ?? themes.light);
+      
+        const handleChangeTheme = () => {
+          console.log(theme);
+          theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light)
+        }
+      
+        useEffect(() => {
+          localStorage.setItem('theme', JSON.stringify(theme));
+        }, [theme])
+      
+    return(
+    <GlobalContext.Provider value={{theme, handleChangeTheme, dentists}}>
+        {children}
+    </GlobalContext.Provider>
+    )
+};
+export default GlobalContextProvider;
