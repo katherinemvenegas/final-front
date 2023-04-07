3d3fbfd6d35c8f27c054f5b3854949e99c5adaef
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-07 18:35:17 -0300

    Se elimina comentarios

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -7,7 +7,7 @@ const ContactForm = () => {
   const [email, setEmail] = useState("");
   const [showCardSuccess, setShowCardSuccess] = useState(false);
   const [error, setError] = useState("");
-  
+
   const validRegex = /^[a-z0-9]+@[a-z]+\.[a-z]+$/;
   const isValidName = name.length > 5;
 
@@ -15,19 +15,13 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(isValidName && isValidEmail){
+    if (isValidName && isValidEmail) {
       setShowCardSuccess(true);
       setError("");
-    }else{
+    } else {
       setError("Por favor verifique su información nuevamente")
       setShowCardSuccess(false)
     }
-    //enviar os dados do formulário e enviá-los no corpo da requisição 
-    //para a rota da api que faz o login /auth
-    //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
-    //no localstorage para ser usado em chamadas futuras
-    //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
-    //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
   };
   const onNameChange = (e) => {
     setName(e.target.value);
@@ -38,8 +32,6 @@ const ContactForm = () => {
 
   return (
     <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
       <div>
         <div className={`card-body ${styles.CardBody}`}>
           <form onSubmit={handleSubmit}>
@@ -65,9 +57,8 @@ const ContactForm = () => {
             </button>
             <div>{error}</div>
           </form>
-          
         </div>
-        {showCardSuccess ? <CardSuccess name = {name}/> : null}
+        {showCardSuccess ? <CardSuccess name={name} /> : null}
       </div>
     </>
   );
