595b9f7cfc24266b991847b965e6cdccf0449812
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-07 19:04:40 -0300

    Agrego GlobalContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import ThemeContext from "./context";
-
-export const themes = {
-  light: {
-    font: "black",
-    background: "white"
-  },
-  dark: {
-    font: "white",
-    background: "black"
-  }
-}
+import GlobalContextProvider from "./context";
 
 function App() {
-  const [theme, setTheme] = useState(themes.light)
-
-  const handleChangeTheme = () => {
-    console.log(theme);
-    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light)
-  }
   return (
     <>
-      <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
+      <GlobalContextProvider>
         <div>
           <Navbar />
           <main>
@@ -33,7 +14,7 @@ function App() {
           </main>
           <Footer />
         </div>
-      </ThemeContext.Provider>
+      </GlobalContextProvider>
     </>
   );
 }
