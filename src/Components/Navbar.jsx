1af28454b05108823117e0b439e02964bb971523
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-08 21:44:12 -0300

    Se eliminan comentarios

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,14 +8,11 @@ const Navbar = () => {
 
   return (
     <header className="sticky-top">
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar navbar-dark bg-dark ou navbar-light bg-light*/}
       <nav
         className={`navbar navbar-expand-sm navbar-light bg-light`}
         aria-label="Third navbar example"
       >
         <div className="container">
-          {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
           <a className={`navbar-brand ${styles.navbarBrand}`} href="/home">
             DH Odonto
           </a>
@@ -37,33 +34,23 @@ const Navbar = () => {
           >
             <ul className="navbar-nav mb-2 mb-sm-0">
               <li className={`nav-item ${styles.navBarLink}`}>
-                {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
                 <a className="nav-link" href="/home">
                   Home
                 </a>
               </li>
               <li className={`nav-item ${styles.navBarLink}`}>
-                {/* Se o usuário estiver logado, deverá aparecer um botão de logout
-                que vai apagar o token do localstorage.
-                Se o usuário estiver deslogado, um link fará um redirecionamento, com react-router,
-                ao formulário de login
-                O botão de logout deverá ser testado darkmode
-                se sim, btn-dark, se não, btn-light */}
+               
                 <a className="nav-link" href="/contact">
                   Contact
                 </a>
               </li>
               <li className={`nav-item ${styles.navBarLink}`}>
-                {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
+    
                 <a className="nav-link" href="/favs">
                   Favs
                 </a>
               </li>
               <li className={`nav-item`}>
-                {/* Ao ser clicado, esse botão mudará a aplicação para dark mode ou light mode.
-                 Lembre-se de usar um estado no contexto para fazer essa alteração.
-                 Na linha seguinte deverá ser feito um teste se a aplicação
-                 está em dark mode e deverá utilizar o icone ☀ ou 🌙 e btn-dark ou btn-light*/}
                 <button
                   onClick={() => dispatchTheme({ type: "toggle_theme" })}
                   className={`btn btn-light${styles.btnStyle
