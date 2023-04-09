5e6a93630afbadb0fe2b987db4698fe010662e05
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-08 21:46:30 -0300

    Se eliminan comentarios e import sin usar

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,25 +1,19 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Card from "../Components/Card";
 import { GlobalContext } from "../context";
 
-const Home = ({children}) => {
-  const {stateTheme}= useContext(GlobalContext);
-
-  useEffect(() => {
-    //Nesse useEffect, deverá ser obtido todos os dentistas da API
-    //Armazena-los em um estado para posteriormente fazer um map
-    //Usando o componente <Card />
-  }, []);
+const Home = ({ children }) => {
+  const { stateTheme } = useContext(GlobalContext);
 
   return (
     <>
-    <div  style={{  background: stateTheme.background, color: stateTheme.font}}>
-    {children}
-      <h1>Home</h1>
-      
-      <div className="card-grid container">
-        <Card />
-      </div>
+      <div style={{ background: stateTheme.background, color: stateTheme.font }}>
+        {children}
+        <h1>Home</h1>
+
+        <div className="card-grid container">
+          <Card />
+        </div>
       </div>
     </>
   );
