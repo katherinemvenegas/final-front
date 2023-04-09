5e0f97f28c481ab9bff7930e17f3cdcf9632457c
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-08 21:31:40 -0300

    Se agrega componente CardFavs

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,15 +1,17 @@
 import { useContext } from "react";
+import CardFavs from "../Components/CardFavs";
 import { GlobalContext } from "../context";
-const Favs = ({children}) => {
-    const {stateTheme} = useContext(GlobalContext);
-    return(
-        <>
-          <div style={{  background: stateTheme.background, color: stateTheme.font}}>
-            {children}
-            <h1>Favs</h1>
-          </div>
-        </>
-      );
+const Favs = ({ children }) => {
+  const { stateTheme } = useContext(GlobalContext);
+  return (
+    <>
+      <div style={{ background: stateTheme.background, color: stateTheme.font }}>
+        {children}
+        <h1>Favs</h1>
+        <CardFavs />
+      </div>
+    </>
+  );
 }
 
 export default Favs;
