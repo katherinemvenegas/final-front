490fe3b57040c9129c435690f4bc170c1c55d5aa
katherinemvenegas <katherine.m.venegas@gmail.com>
2023-04-08 21:42:12 -0300

    Se eliminan comentarios

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,14 +8,10 @@ const Footer = () => {
     <footer>
       <div className={styles.footerWrapper}>
         <button className={`btn btn-danger ${styles.top}`} onClick={scrollToTop}>Back to top</button>
-        {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar a class navbar-dark bg-dark ou navbar-light bg-light  */}
         <div className={`navbar-light bg-light} ${styles.footer}`}>
           <div className="container">
             <div className={`row`}>
               <div className="col-sm-12 col-lg-6">
-                {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-                // está em dark mode e deverá utilizar o css correto */}
                 <img className={`${styles.dhLogo}`} src="/images/DH.png" alt='DH-logo' />
               </div>
               <div className={`col-sm-12 col-lg-6 ${styles.icons}`}>
